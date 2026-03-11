cf8978b10635c3daef3d573f4aa4554fea3144c4
So6oN_Maximix <maxime.leost@gmail.com>
2026-03-11 16:33:24 +0100

    Loading Username Test - Corrections 2

--- profile.js
+++ profile.js
@@ -13,7 +13,7 @@ async function checkAuthentification() {
     if (username) {
         console.log(username);
         usernameElement.textContent = username;
-        avatarLettersElement = username.toString()[0].toUpperCase();
+        avatarLettersElement = username.split("")[0].toUpperCase();
     } else {
         if (window.location.pathname === "/profile") window.location.href = "/login";
     }
