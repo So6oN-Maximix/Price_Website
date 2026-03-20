d4ac2e6fee1b66bd5b3c20ba5c67e816932efda9
So6oN_Maximix <maxime.leost@gmail.com>
2026-03-20 09:24:12 +0100

    Adding Settings et Orders Profil Menus

--- profile.js
+++ profile.js
@@ -1,3 +1,13 @@
+const dashboardViewer = document.getElementById("dashboard-viewer");
+const ordersViewer = document.getElementById("view-orders");
+const settingsViewer = document.getElementById("view-settings");
+const viewers = [dashboardViewer, ordersViewer, settingsViewer];
+
+const dashboardTabBtn = document.getElementById("tab-dashboard");
+const ordersTabBtn = document.getElementById("tab-orders");
+const settingsTab = document.getElementById("tab-settings");
+const menuButtons = [dashboardTabBtn, ordersTabBtn, settingsTab];
+
 function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
@@ -9,6 +19,7 @@ async function checkAuthentification() {
     const avatarLettersElement = document.getElementById("avatar-letters");
     const usernameElement = document.getElementById("username");
     const username = getCookie("username");
+    const profileLink = document.getElementById("nav-profile-link");
     const cartElement = document.getElementById("nav-cart-link");
     if (username) {
         if (profileLink) profileLink.href = "/profile";
@@ -20,11 +31,29 @@ async function checkAuthentification() {
     }
 }
 
-const profileLink = document.getElementById("nav-profile-link");
-
 window.addEventListener("DOMContentLoaded", checkAuthentification);
 document.getElementById("logout-btn").addEventListener("click", () => {
     document.cookie = "session_id=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
     window.location.href = "/";
+});
+
+menuButtons.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        btn.classList.add("active");
+        menuButtons.forEach((button, idx) => {
+            if (idx != index) {
+                if (button.classList.contains("active")) {
+                    button.classList.remove("active");
+                }
+            }
+        });
+        viewers.forEach((viewer, idx) => {
+            if (idx === index) {
+                viewer.style.display = "flex";
+            } else {
+                viewer.style.display = "none";
+            }
+        });
+    });
 });
